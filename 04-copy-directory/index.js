55a87176802c80ce8030ab92fce342a45c8b089c
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2022-05-26 22:22:17 +0300

    fix: 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,35 +1,37 @@
 const fs = require('fs');
 const path = require('path');
-
+let pathCopyFrom = path.join(__dirname, "files");
+let pathCopyTo = path.join(__dirname, "files-copy");
 console.log('started')
 
 const newPath = path.join(__dirname, "files-copy");
 
-async function mkDir () { 
+// async function rmDir() {
+//   await fs.promises.rm(newPath, { recursive: true, force: true });
+// }
+// rmDir();
+
+async function mkDir() {
+  await fs.promises.rm(newPath, { recursive: true, force: true });
   await fs.promises.mkdir(newPath, { recursive: true });
 }
 
-mkDir();
   
-let pathCopyFrom = path.join(__dirname, 'files');
-let pathCopyTo = path.join(__dirname, 'files-copy');
-let filesInDir;
 async function ReadMyDir() {
-  filesInDir = await fs.promises.readdir(pathCopyFrom);
-  filesInDir.forEach(el => {
-    console.log(`${el} works`)
-    copyFileFunc(el);
-  });
-  
+  await mkDir();
+  let filesInDir = await fs.promises.readdir(pathCopyFrom);
+  console.log(filesInDir);
+  for (let file of filesInDir) { 
+    await copyFileFunc(file);
+  }
 }
 
-console.log(ReadMyDir());
-
+ReadMyDir();
 async function copyFileFunc(fileName) {
   const pathToFileCopy = path.join(pathCopyTo, fileName);
-  console.log(pathToFileCopy);
+  // console.log(pathToFileCopy);
   const pathFromFileCopy = path.join(pathCopyFrom, fileName);
-  console.log(pathFromFileCopy);
+  // console.log(pathFromFileCopy);
   await fs.promises.copyFile(pathFromFileCopy, pathToFileCopy);
 }
 
